b58ed7a645badf2fc76ef89592582d04406ac56d
wangshouren <1115808717@qq.com>
2024-08-26 13:17:58 +0800

    feat: remove default value

--- src/hooks/observable.ts
+++ src/hooks/observable.ts
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { Observable } from "rxjs";
 import { useSubscribe } from "./subscribe";
 
-export function useObservable<T>(observable: Observable<T>, defaultValue: T) {
-  const [value, setValue] = useState<T>(defaultValue);
+export function useObservable<T>(observable: Observable<T>) {
+  const [value, setValue] = useState<T>();
   useSubscribe(observable, setValue);
   return value;
 }
